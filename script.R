d477b3f5cdba7b6f2955ded6aed4a43fde40ff95
unknown <mhtran@HIEN-VAIO.leerubber.com.sg>
2015-05-12 17:45:14 +0800

    script.R

--- script.R
+++ script.R
@@ -16,16 +16,16 @@ data <- read.csv(datafile, sep=",", colClasses=c("numeric", "Date", "numeric"), 
 ## hist(dayly_total)
 ## should consider to include NA as zero or not 
 library(dplyr)
-daily_stat <- summarise(group_by(data,date=as.factor(date)), total=sum(steps,na.rm=TRUE), avg=mean(steps), med=median(steps))
-hist(daily_stat$total, breaks = 20)
+date_stat <- summarise(group_by(data,date=as.factor(date)), total=sum(steps,na.rm=TRUE), avg=mean(steps), med=median(steps))
+hist(date_stat$total, breaks = 20)
 ## command to display mean and median here
 
 # 3. What is the average daily activity pattern?
-intv_stat <- summarise(group_by(data,interval), average=mean(steps,na.rm=TRUE))
+int_stat <- summarise(group_by(data,interval), average=mean(steps,na.rm=TRUE))
 ## draw time series
-with(intv_stat, plot(strptime(sprintf("%04d",interval),"%H%M"),average, type="l"))
+with(int_stat, plot(strptime(sprintf("%04d",interval),"%H%M"), average, type="l"))
 ## max
-intv_stat[(intv_stat$average==max(intv_stat$average)),"interval"]
+int_stat[(int_stat$average==max(int_stat$average)),"interval"]
 
 # 4. Imputing missing values
 ## number of rows with NA
@@ -33,11 +33,15 @@ nrow(data[is.na(data$steps),])
 ## devise the strategy to fill in the value?
 ## check to see any row with NA from intv_stat
 ## replace NA in data with average value in int_stat, can use apply() or by()
-## we use for
+## we use for loop
 data1 <- data
 for (i in 1:nrow(data1)) {
-  if (is.na(data1[i,]$steps)) {data1[i,]$steps <- intv_stat[(intv_stat$interval == data1[i,]$interval),"average"]}
+  if (is.na(data1[i,]$steps)) {data1[i,]$steps <- as.numeric(int_stat[(int_stat$interval == data1[i,]$interval),"average"])}
 }
-
+## get new sum, median and mean
+date_stat1 <- summarise(group_by(data1,date=as.factor(date)), total=sum(steps), avg=mean(steps), med=median(steps))
+## hist of total steps per day after imputing NA values
+hist(date_stat1$total, breaks = 20)
+View(date_stat1[,c("date","avg","med")])
 
 
